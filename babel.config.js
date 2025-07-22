1a7796f4a0c6375f184aaf5bd2ab3156f2550c35
hank <hank29206880@gmail.com>
2025-07-22 17:29:12 +0900

    chore: change config

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,16 @@
 module.exports = {
-	presets: [
-		['@babel/preset-env', {
-			targets: {
-				chrome: '58',
-				ie: '11',
-			},
-			useBuiltIns: 'usage',
-			corejs: {
-				version: 3,
-			}
-		}]
-	]
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          chrome: "58",
+        },
+        useBuiltIns: "usage",
+        corejs: {
+          version: 3,
+        },
+      },
+    ],
+  ],
 };
